d3010dcc27ba5dbd5717a817678df62da23e8c3c
Venkat <psai96526@gmail.com>
2026-05-04 13:33:45 +0530

    imp

---
+++ tfvars/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow-1.id]
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name}-env"
+    }
+  )
+}
+
+resource "aws_security_group" "allow-1" {
+  name = Name = "${local.common_name}-sg"
+
+  ingress {
+    from_port = var.ingress_from_port
+    to_port = var.ingress_to_port
+    protocol = var.protocol
+    cidr_blocks = var.cidir
+  }
+  
+  egress {
+    from_port = var.egress_from_port
+    to_port = var.egress_from_port
+    protocol = var.protocol # from all protocols
+    cidr_blocks = var.cidir
+  }
+   tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name}-sg"
+    }
+  )
+}
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket         = "remote-state-88-prod"
+key            = "terraform-demo"
+use_lockfile = true
+encrypt = true
+region         = "us-east-1"
